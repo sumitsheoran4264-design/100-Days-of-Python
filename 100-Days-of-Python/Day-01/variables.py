f2d5f2310bc26be32e423f5fd2e0b824e7bec830
Sumit <sumit@example.com>
2026-05-10 11:57:47 +0530

    Stop tracking .ink files and update progress

--- 100-Days-of-Python/Day-01/variables.py
+++ 100-Days-of-Python/Day-01/variables.py
@@ -1,8 +1,5 @@
-# name = "sumit"
-# print(len(name))
-# name ="sumit kumar"
-# print(len(name))
-# input("enter your name: ")
-username = input("What is your name? ")
-length = len(username)
-print(length)
+name = "sumit"
+print(len(name))
+name ="sumit kumar"
+print(len(name))
+input("enter your name: ")
