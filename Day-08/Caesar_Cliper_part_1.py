f59f4f003688f59ce3a85b0decfa3945fcbe7e07
Sumit <https://github.com/sumitsheoran4264-design/100-Days-of-Python>
2026-05-25 13:22:16 +0530

    updated Day-08

--- Day-08/Caesar_Cliper_part_1.py
+++ Day-08/Caesar_Cliper_part_1.py
@@ -4,13 +4,14 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#Hello, 2
 def encrypt(orginial_text, shift_amount):
     cipher_text = ""
                    
     for letter in orginial_text:
         shifted_position = alphabet.index(letter) + shift_amount # letter + shifted_amount = shift_position
+        shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
+        
     print(f"Here is the encode result: {cipher_text}")
 
 
